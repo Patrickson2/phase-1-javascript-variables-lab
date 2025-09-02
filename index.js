bfea7eea9627e87430800dd0c0de9e5e33d71a88
Patrickson2 <thairupatrickson@gmail.com>
2025-09-02 14:36:29 +0300

    added solutions

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+let companyName = 'Scuber';{
+    const companyName ='scuner';
+}
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
