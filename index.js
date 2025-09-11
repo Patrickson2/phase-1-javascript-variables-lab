c2c61c03e6b238457fe38dc85acf84ba6cbd80b0
Patrickson2 <thairupatrickson@gmail.com>
2025-09-11 14:04:18 +0300

    added variable

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file!
-let companyName = 'Scuber';{
-    const companyName ='scuner';
-}
+const companyName ='Scuber';
+
 let mostProfitableNeighborhood = 'Chelsea';
 
 let companyCeo = 'Susan Smith';
